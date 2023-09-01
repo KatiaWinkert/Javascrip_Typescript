e694b5ddaf56c04feacc0d14b9cac44612235d8b
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-01 19:21:36 -0300

    :book:  Exercicios comentarios
    como fazer comentarios

---
+++ aula03/comentarios.js
@@ -0,0 +1,13 @@
+// e escrever um comentário
+console.log("olá mundo!"); // aqui tem outro comentario. 
+
+console.log("linha 1")
+console.log("linha 2")
+console.log("linha 3")  
+console.log("linha 4")
+//vai ser ignorado
+
+// -> comentario de linha 
+/* comentario 
+de
+ bloco*/ 
