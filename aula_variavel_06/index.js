be67fa3db49aedb6b546e1ca7c8e48a8cbe1a7be
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-03 20:10:15 -0300

    :book: Aula 06
    Aula sobre Let

---
+++ aula_variavel_06/index.js
@@ -0,0 +1,22 @@
+/*let nome = 'João'; // entre aspas é string
+let nomeDois = 'Maria';
+
+console.log(nome, 'nasceu em 1984.');
+console.log('em 2000', nome, 'conheceu.');
+console.log(nome, 'casou-se com ', nomeDois,'em 2012.');
+console.log(nomeDois, 'teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');*/
+
+/*let name; //declarou a variavel => retorna valor undefined 
+name = 'katia' // incializando uma variavel 
+console.log(name);
+name = 'Bruna';
+console.log(name)*/
+
+// Não podemos criar variaveis com palavras reservadas tipo => let let = 
+//variaveis precisam ter nomes significativos => let n = 'João'
+let nomeCliente = 'João'
+console.log(nomeCliente) 
+//não podemos ter uma variavel com numeros 
+// Não pode ter variaveis com espaços. => ler nome da =  o correto é let nomeDa = / Utilizamos camelCase. 
+//variaveis são case-sensitive ex: "let nomeCliente=''" e "let nomecliente='' " são diferentes devido a letra do camelCase. 
