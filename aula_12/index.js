292931fba6fe9ab962ac9b5923a94bb5ea6064ae
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-04 11:11:21 -0300

    :bookmark_tabs: Exercicios
    Atividade: incluir valor de consta através do prompt,  Transformar strings em number fazer um resultado e mostrar por alert

---
+++ aula_12/index.js
@@ -0,0 +1,9 @@
+let num1 = prompt ('Digite o primeiro numero.');
+let num2 = prompt ('Digite o segundo numero');
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+const resultado =  num1 + num2;
+
+alert(`O resultado da sua conta foi: ${resultado}`) //=> usa templete string para exibir resultado com frases no alerta por exemplo.
