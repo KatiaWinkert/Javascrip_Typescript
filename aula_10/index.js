0a66113a96ecefb8114beab3a690244cbd4d3a9e
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-04 11:08:25 -0300

    :book: index.js Strings
    Aula sobre strings e seus tipos

---
+++ aula_10/index.js
@@ -0,0 +1,12 @@
+//Tipos de dados primitivos. => String, number, undefined, null, boolean, symbol.
+
+const nome = 'Bianca'; // => são strings (Texto).
+const nome1 =  "Bianca"; // => são strings (Texto).
+const nome2 = `Bianca`; // => são strings (Texto). 
+const num1 = 10; // => number (numeros).
+const num2 = 10.5; // => number (numeros).
+let nomeAluno; // => undefined - valor indefinido - Não aponta a lugar nenhum na memória.
+const sobreNomeAluno = null; // => Nulo -> não aponta para local nenhum na memória
+const boolean = true; // Boolean => true , false (logico) 
+
+console.log(typeof nome, nome)
