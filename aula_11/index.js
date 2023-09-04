5a20950dc39fed71f7c76eae8b96fade4b768cb9
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-04 11:09:20 -0300

    :book: Aula 11
    Aula sobre operadores aritméticos e suas procedencias

---
+++ aula_11/index.js
@@ -0,0 +1,60 @@
+/* Operadores Aritmeticos 
+
+    => + Adição / Concatenação => unir dois valores (510)
+    => - / * 
+    => ** potenciação =. elevar um numeroi exemplo : dois elevado a 10 
+    => %  Resto da divisão. 
+
+    Precedencia : 
+    => () primeiro tudo que esta entre parentese,  
+    => ** potencialização,
+    => *, / ,% . multiplicação, divisão, resto,
+    => +, -  soma e subtração. 
+
+    => operador de incremento ++ 
+    => operador de decremento --
+
+*/ 
+
+/*const num1 = 5; 
+const num2 = 2; 
+const num3 = 10;
+
+console.log((num1+num2) * num3)*/
+
+let contador = 15; 
+contador++;
+contador++;
+contador++;
+contador++;
+
+
+let contador1 = 10; 
+
+console.log(--contador1)
+
+console.log(contador)
+
+/* Nan - Not a number => quando aparece dessa forma normalmente ocorreu um erro, 
+as declarações da variavel nao compente para dar um resultado exato, exemplo: 
+    const number = 1; 
+    const number = "L4"; 
+    => ao complitar gera um Nan como resultado (error). 
+
+    para transformar uma string em forma numerica podemos usar alguns comados, os mais usados aseria
+    => parseInt (interiro)
+    => parseFloat(decimais) 
+    exemplo: 
+    const number = 1; 
+    const number = parseInt('4'); 
+
+    const number = 1; 
+    const number = parceFloat('4.5');
+
+    Ou entao simplemente podemos usar o Number que o mesmo faz a conversão, o mesmo da para numero inteiro ou flutuante.
+    const number = 1; 
+    const number = Number ('5.2');
+    
+
+
+*/
