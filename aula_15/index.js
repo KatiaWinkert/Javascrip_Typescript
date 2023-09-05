9d61ba450f884ce20f0a05b869f55448065bdf81
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-05 20:28:56 -0300

    :book: Aula 15
    Aula sobre Math e seus metodos.

---
+++ aula_15/index.js
@@ -0,0 +1,21 @@
+let num1 = 9.5; 
+console.log(num1) 
+
+
+
+let num2 = Math.floor(num1) // Math. floor => aredonda o numero para baixo 
+//let num3 = Math.ceil(num1) // Math.ceil => aredonda o numero para cima 
+//let num4 = Math.round(num1) // Math.round => aredonda o numero para cima ou para cima, verificando a partir da metade.
+//console.log(num3)
+
+//console.log(Math.max(1,2,3,4,5,10,50,46,-20,500,344,-5648,86487,2345,-4567)) // metodo que pega o maior numero 
+//console.log(Math.min(1,2,3,4,5,10,50,46,-20,500,344,-5648,86487,2345,-4567)) // metodo que pega menor numero
+
+
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5) // metodo round arredonda, metodo random faz numeros aleatorios
+//console.log(aleatorio) // metodo que gera numeros aleatorios 
+
+//console.log(Math.PI) // metodo valor de PI 
+//console.log(Math.pow(2,10)) // metodo de potenciação 
+//console.log(2 ** 10 ) // potenciação => pode ser usada dessas duas formas. */
+
