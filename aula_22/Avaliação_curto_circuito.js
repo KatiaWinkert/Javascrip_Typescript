91d2f7d753942f10ed7cea06ecf5a8a09d30aaa8
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-07 19:35:59 -0300

    :book: Avaliação de curto circuito
    Avaliação de curto circuito

---
+++ aula_22/Avaliação_curto_circuito.js
@@ -0,0 +1,14 @@
+/*
+&& -> false && true -> false "o valor mesmo"
+|| -> true || false -> vai retornar o valor verdadeiro
+FALSY
+false
+0
+'' "" ``
+null / undefined 
+NaN 
+qual quer desses a cima o javascript avalia como falso o restante será tudo verdadeiro. 
+
+
+*/
+console.log('Luiz' &&  null && 'Maria')
