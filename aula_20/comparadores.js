e3e4e20d21cda0ec0c486cb52720a9f6112c5a17
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-07 19:34:51 -0300

    :book: Aula 20
    Operadores de comparação

---
+++ aula_20/comparadores.js
@@ -0,0 +1,16 @@
+/*
+Operadores de comparação : 
+>  maior que 
+>= maior que ou igual a 
+< menor que 
+<= menor que ou igual a 
+== igualdade // => esse verifica valor -> não é muito recondado a utilização. ************
+=== igualdade estrita // verifica valor e tipo. 
+!= diferente // checa valor não é muito recondado a utilização. ****** 
+!== diferente estrito // checa valor e tipo
+*/
+const num1 = 10;
+const num2 = '11';
+
+const comp = num1 !== num2;
+console.log(comp)
