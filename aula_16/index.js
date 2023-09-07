46a1adb6738420c957324f9b861a5bead7707f67
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-06 22:06:35 -0300

    :book: Aula 16
    Aula sobre arryas seus metodos e funcionalidades.

---
+++ aula_16/index.js
@@ -0,0 +1,31 @@
+
+// indeces     0         1       2
+let alunos = ['Luiz', 'Maria', 'João'] // arrays sempre são criados com colchetes, cada objeto dentro dele tem um indice. 
+
+const removido = alunos.pop();
+alunos.pop('João')
+
+//alunos.unshift('Luiza') // adiociona no começo do array 
+//alunos.unshift('Bruno')
+console.log(removido)
+console.log(alunos)
+
+//alunos.push('Claudio')
+//alunos.push('Bianca') => adiciona no fim do array 
+
+
+//alunos[alunos.length] = 'Luiza'
+//alunos[alunos.length] = 'Fabio' => adiciona no fim do array 
+//alunos[alunos.length] = 'Luana'
+//console.log(alunos)
+
+
+//alunos[0] = 'Eduardo' => altera array 
+//alunos[3] = 'Luiza'
+
+
+
+//console.log(alunos)
+//console.log(alunos[1])
+//console.log(alunos[2])
+//console.log(alunos)
