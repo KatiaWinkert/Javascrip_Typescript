1de8bee5cf7bacb5da4557a1711fdb619c669c61
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-13 22:43:16 -0300

    :book: aulas

---
+++ aula_23/if_else.js
@@ -0,0 +1,10 @@
+// entre e 0 e 11 hrs bom dia / 12 -17 boa tarde / 18 -23 boa noite
+
+const hora = 13 
+
+if(hora =>  0 && hora <= 11){
+    console.log('Bom dia!')
+} else if () {
+    console.log('Boa tarde')
+}
+
