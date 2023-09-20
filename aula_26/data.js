ae25c75937288dd01102a12b5aa31ff852286b91
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-20 20:15:59 -0300

    :book: Data.js
    Aula sobre Função Date

--- aula_26/data.js
+++ aula_26/data.js
@@ -2,5 +2,34 @@
 //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date
 // javascript o mes começa a ser contado do 0 ou seja 0 = janeiro por isso o mes de maio é 4 
 
-const data = new Date(2020, 4, 10, 10, 30, 500); // ano, mes, dia, hora, segundos e milesiomo de segundo 
-console.log(data.toString())
+//const data = new Date(2020, 4, 10, 10, 30, 500); // ano, mes, dia, hora, segundos e milesiomo de segundo 
+
+//const data = new Date('2023-09-19 19:40:25')
+//console.log('Dia', data.getDate())
+//console.log('Mês', data.getMonth() + 1)
+//console.log('Ano', data.getFullYear())
+//console.log('Hora', data.getHours())
+//console.log('Min', data.getMinutes())
+//console.log('Sec', data.getSeconds())
+//console.log('ms', data.getMilliseconds())
+//console.log('Dia da semana', data.getDay())
+//console.log(data.toString())
+
+function zeroAesquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formaData (data){
+const  dia = zeroAesquerda (data.getDate()); 
+const mes = zeroAesquerda (data.getMonth() + 1);
+const   ano = zeroAesquerda (data.getFullYear());
+const   hora = zeroAesquerda (data.getHours());
+const   min = zeroAesquerda (data.getMinutes());
+const   sec = zeroAesquerda (data.getSeconds());
+
+return`${dia}/${mes}/${ano} ${hora}:${min}:${sec}`
+}
+
+const data = new Date();
+const dataBrasil = formaData(data)
+console.log(dataBrasil)
