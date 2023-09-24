a17df9c72b11763e6652b65805e792107b49df50
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-24 17:31:52 -0300

    :book: Estrutura de repetição
    Sobre for classico e for in

---
+++ aula_31/estrutura_repeticao.js
@@ -0,0 +1,31 @@
+const pessoa = {
+    nome: 'katia',
+    sobrenome: 'Winkert',
+    idade: 34
+};
+
+for(let i in pessoa){
+    console.log(i, pessoa[i])
+}
+
+//console.log(pessoa.nome); //
+//console.log(pessoa['nome']);
+
+//for(let key in pessoa){
+//    console.log(key)
+//}
+
+
+
+
+
+// for in le os indices ou chaves do objeto
+//for(let i in frutas) {
+//    console.log( frutas[i]);
+//}
+
+
+
+//for(let i = 0 ; i < frutas.length; i ++){
+//    console.log(frutas[i])
+//}
