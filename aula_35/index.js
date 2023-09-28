f8f1f0e11fe276b55f1a6e577633c8ceb389883c
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-28 00:03:39 -0300

    :bookmark: Exec 35
    // Escreva uma função que receba 2 números e retorne o maior deles.

---
+++ aula_35/index.js
@@ -0,0 +1,16 @@
+// Escreva uma função que receba 2 numeros e retorne o maior deles. 
+
+//function numeros (min, max){
+//    if(min > max){
+//        return min;
+//    } else {
+//        return max;
+//    }
+//}
+
+const numeros = (min, max) => min > max ? min : max;
+
+
+//const maior = numeros(5,2);
+//console.log(maior);
+console.log(numeros(7,5))
