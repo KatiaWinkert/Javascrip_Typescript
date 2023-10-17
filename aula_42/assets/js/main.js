e1967947de63e660c0c0bd055923aedda6072d4c
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-10-17 00:22:20 -0300

    :bookmark_tabs: Exec
    Finalizando atividade lista

--- aula_42/assets/js/main.js
+++ aula_42/assets/js/main.js
@@ -15,18 +15,21 @@ inputTarefa.addEventListener('keypress', function(e){
     }
 })
 
-//função para limpar input: 
+//função para limpar input: focus => serve para ficar a barrinha piscando no input 
 function limpaInput(){
     inputTarefa.value = ""; 
     inputTarefa.focus();
 
 }
 
-//função para apagar lista
+//função para criar botão de apagar item da lista: 
 function criaBotaoApagar (li) {
     li.innerText += ' '; // =>  espaço no texto
     const botaoApagar = document.createElement('button'); 
     botaoApagar.innerText = 'Apagar';
+    //botaoApagar.classList.add('apagar'); uma opção para add class;
+    botaoApagar.setAttribute('class', 'apagar'); // outra opção para formar uma class 
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
     li.appendChild(botaoApagar);
 
 }
@@ -36,6 +39,8 @@ function criaTarefa(textoInput) {
     li.innerText = textoInput;
     tarefas.appendChild(li)
     limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
 }
 
 
@@ -44,3 +49,43 @@ btnTarefa.addEventListener('click',  function(){
     if(!inputTarefa.value) return;
    criaTarefa(inputTarefa.value)
 })
+
+//função para apagar :  
+document.addEventListener('click', function(e){
+    const el = e.target; 
+
+    //se esse botão contem a classe 'apagar'
+    if(el.classList.contains('apagar')){
+        //do elemento o pai dele será removido()
+        el.parentElement.remove(); 
+        salvarTarefas();
+    }
+
+})
+
+//função para salvar tarefas : 
+function salvarTarefas (){
+    const listaTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of listaTarefas){
+        let  tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+
+}
+
+//função que vai ler as tarefas e joga-las de volta na ul: 
+function adicionarTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa)
+    }
+}
+
+adicionarTarefasSalvas();
