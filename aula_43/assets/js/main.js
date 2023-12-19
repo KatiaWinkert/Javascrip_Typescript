cf88f23ac57006f08e8cb3535862ecdba07e967f
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-12-18 21:28:41 -0300

    :construction: Js
    javascript commands

--- aula_43/assets/js/main.js
+++ aula_43/assets/js/main.js
@@ -1,1 +1,23 @@
-//alert(1);
+function criaCalculadora() {
+    return{
+// atributos que são variaveis do obj vao para cima 
+        display: document.querySelector('.display'),
+
+
+
+        inicia() {
+            alert("oi")
+        },
+//tudo que dor metodo vem para baixo 
+
+        cliqueBotoes(){
+            document.addEventListener('click', function(e) {})
+
+        },
+
+
+    }
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia()
