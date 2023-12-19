54c40fb0fe625bbd48d46b9ad28a09d4e68db3e7
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-12-19 00:16:25 -0300

    :alien:
    "compile-hero.disable-compile-files-on-did-save-code": false

--- aula_43/assets/js/main.js
+++ aula_43/assets/js/main.js
@@ -3,21 +3,89 @@ function criaCalculadora() {
 // atributos que são variaveis do obj vao para cima 
         display: document.querySelector('.display'),
 
+       
 
 
         inicia() {
-            alert("oi")
+            this.cliqueBotoes();
+            this.pressionaEnter();
         },
 //tudo que dor metodo vem para baixo 
 
-        cliqueBotoes(){
-            document.addEventListener('click', function(e) {})
+        pressionaEnter(){
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13){
+                    this.realizaConta();
+                }
+            })
 
         },
 
+
+        //metodo para limpar display 
+        clearDispley(){
+            this.display.value = '';
+        },
+
+        realizaConta(){
+            const conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta){
+                    alert('Conta inválida!');
+                    return;
+                }
+
+                this.display.value = String(conta);
+                
+            } catch (error) {
+                alert('Conta inválida!');                
+                return;
+            }
+
+        },
+
+        //apagaum é para apagar um numero da calculadora 
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1) 
+        },
+
+        //metodo para incluir evento (adicionar numero para os calculos)
+        cliqueBotoes(){
+            //this > calculadora 
+            document.addEventListener('click', e => {
+                const el = e.target; 
+
+            if(el.classList.contains('btn-num')) {
+                this.btnParaDisplay(el.innerText);
+            }
+
+            if(el.classList.contains('btn-clear')){
+                this.clearDispley()
+            }
+
+            if(el.classList.contains('btn-del')){
+                this.apagaUm();
+            }
+
+
+            if(el.classList.contains('btn-eq')){
+                this.realizaConta()
+            }
+
+            }); 
+                
+
+        },
+btnParaDisplay(valor){
+    this.display.value += valor;
+
+} 
 
     }
 }
 
-const calculadora = criaCalculadora()
+const calculadora = criaCalculadora();
 calculadora.inicia()
