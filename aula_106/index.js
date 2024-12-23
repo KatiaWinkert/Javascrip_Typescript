1a9b1f2f91d22ffdb56d9181f63941cdd1b2c308
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2024-12-22 22:17:16 -0300

    :construction: Exercicios
    Exercício para validar cpf

--- aula_106/index.js
+++ aula_106/index.js
@@ -1,7 +1,43 @@
+//058.128.389-92 070.987.720-90 cpf 
+
 class ValidaCPF{
     constructor(cpfenviado){
+        
+        // esse processo do objeto abaixo dentro do construtor tem a finalidade de limpar os pontos 
+        //do cpf, ou seja, tudo que não for numero será excluído;
+        //isso é um objeto 
+        Object.defineProperty(this, 'cpflimpo', {
+        // para ocultar ou proteger essa propriedade utiliza os comandos a baixo. 
+        writable: false,
+        enumerable: true,
+        configurable: false,
+        
+            value: cpfenviado.replace(/\D+/g, ''),       
+
+    });
 
     }
+
+        ésequência () {
+        return this.cpflimpo.charAt(0).repeat(11) === this.cpflimpo;
+    } 
+
+    //metodo: 
+    valida() {
+        //se algo for diferebte de cpf retorna falso
+        //cpf diferente de string retorna falso
+        //se cpf for diferente de 11 caracteres retorna falso. 
+        if(!this.cpflimpo) return false;
+        if(typeof this.cpflimpo !== 'string') return false;
+        if(this.cpflimpo.length !== 11) return false; 
+        if(this.ésequência()) return false;       
+        
+        return 'Cheguei Aqui!';
+    }
+
 }
 
-const valida_cpf = new ValidaCPF('');
+
+//instacia: 
+const valida_cpf = new ValidaCPF('070.987.720-90');
+console.log(valida_cpf.valida)
