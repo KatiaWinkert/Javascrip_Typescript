94678f80df40baaa38ba7bb823a3e25e2779de79
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2024-12-23 22:03:13 -0300

    🚧 Update
    Update Class

--- aula_106/index.js
+++ aula_106/index.js
@@ -9,28 +9,49 @@ class ValidaCPF{
         Object.defineProperty(this, 'cpflimpo', {
         // para ocultar ou proteger essa propriedade utiliza os comandos a baixo. 
         writable: false,
-        enumerable: true,
-        configurable: false,
-        
-            value: cpfenviado.replace(/\D+/g, ''),       
+         enumerable: true,
+          configurable: false,
+             value: cpfenviado.replace(/\D+/g, ''),       
 
-    });
+    });    
 
     }
+        geraNovoCpf(){
+            const cpfSemDigitos = this.cpflimpo.slice(0, -2); 
+             const digito1 = this.geraDigito(cpfSemDigitos);
+              const digito2 =  this.geraDigito(cpfSemDigitos + digito1);
+            
+    }
+        geraDigito () {
+            let total = 0 ; 
+             let reverso = cpfSemDigitos.length + 1 ; 
 
+            for(let stringNumerica of cpfSemDigitos){
+                total += reverso * Number(stringNumerica); 
+                 reverso--; 
+            }
+            
+            const digito = 11 - (total % 11);
+            return digito <= 9 ? String(digito) : '0'; 
+        
+        }
+
+        //metodo que inibe sequecia numerica 
         ésequência () {
-        return this.cpflimpo.charAt(0).repeat(11) === this.cpflimpo;
+         return this.cpflimpo.charAt(0).repeat(11) === this.cpflimpo;
     } 
 
     //metodo: 
     valida() {
         //se algo for diferebte de cpf retorna falso
         //cpf diferente de string retorna falso
-        //se cpf for diferente de 11 caracteres retorna falso. 
+        //se cpf for diferente de 11 caracteres retorna falso.
+        //se é sequencia retorna falso 
         if(!this.cpflimpo) return false;
         if(typeof this.cpflimpo !== 'string') return false;
         if(this.cpflimpo.length !== 11) return false; 
-        if(this.ésequência()) return false;       
+        if(this.ésequência()) return false;
+        this.geraNovoCpf()       
         
         return 'Cheguei Aqui!';
     }
