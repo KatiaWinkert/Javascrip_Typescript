c2a6fa0357824329dfbee78375bbd4fbc53a727b
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2024-12-28 17:00:37 -0300

    🚧 Update Class
    Update

--- validando_Form/js/index.js
+++ validando_Form/js/index.js
@@ -16,7 +16,25 @@ constructor() {
     handleSubmit(e) {
         e.preventDefault(); 
         const camposValido = this.camposSaoValidos();
+        const senhaValida = this.senhasSãoValidas();
 
+    }
+
+
+
+    senhasSãoValidas(){
+        let valid = true; 
+
+            const senha = this.formulario.querySelector('.senha');
+            const repetirSenha = this.formulario.querySelector('.repetir-senha')
+
+            if(senha.value !== repetirSenha.value){
+                valid = false; 
+                this.criaError(senha, 'Campos senha e repetir senha precisam ser iguais.');
+                this.criaError(repetirSenha, 'Campos senha e repetir senha precisam ser iguais.')
+            }
+
+        return valid;
     }
 
     //esse metodo executa o processo de validar se os campos do formulario foram preenchidos ou nao.
@@ -33,7 +51,7 @@ constructor() {
 
 
         for(let campo of this.formulario.querySelectorAll('.validar')){
-            let label = campo.previousElementSibling.innerHTML;
+            let label = campo.previousElementSibling.innerText;
 
               if(!campo.value){
                 this.criaError(campo, `Campo ${label} não pode estar em branco.`);
@@ -41,16 +59,40 @@ constructor() {
               }
 
               if(campo.classList.contains('cpf')){
-                if(!this.validaCpf(campo)) valid = false; 
+                if(!this.validaCPF(campo)) valid = false; 
+              }
+
+              if(campo.classList.contains('usuario')){
+                if(!this.validaUsuario(campo)) valid = false; 
               }
         }
         
+        return valid;
     }
 
-    validaCpf(campo) {
-        const cpf = new validaCpf(campo.value); 
 
-        if(!cpf.valid()){ 
+    //validação de usuarios e mensagem de error
+    validaUsuario(campo){
+        const usuario = campo.value; 
+        let valid = true;
+
+        if(usuario.length < 3 || usuario.length > 12) {
+            this.criaError(campo, 'Usuário precisa ter entre 3 a 12 caracteres.')
+        }
+
+        // expressão regular: 
+        if(!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+            this.criaError(campo, 'Nome de usuário precisa conter apenas letras ou números.')
+        }
+
+
+        return valid; 
+    }
+
+    validaCPF(campo) {
+        const cpf = new ValidaCPF(campo.value); 
+
+        if(!cpf.valida()){ 
             this.criaError(campo, 'CPF invalido');   
             return false;        
         }
