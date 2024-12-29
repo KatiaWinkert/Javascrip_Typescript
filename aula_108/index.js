1dd84488dfe38348d1d1eada65cf1ed9feac495a
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2024-12-29 20:52:35 -0300

    🚧 Class 108
    Aula sobre promessas (promises)

--- aula_108/index.js
+++ aula_108/index.js
@@ -1,5 +1,7 @@
 //Promises > promessas foi criada para facilitar as callback 
 
+
+//função rand executa o tempo 
 function rand(min , max){
 
     min *= 1000; 
@@ -9,8 +11,14 @@ function rand(min , max){
 }
 
 
+//função esperaAi, que faz o processo da mensagem e do tempo
+// if trata o cacth > Erro 
+//setTimeout trata o resolve. 
 function esperaAi (msg, tempo ) {
     return new Promise(( resolve, reject) => {
+
+        if(typeof msg !== 'string') reject (new Error);
+
         setTimeout(( ) => {
             resolve(msg)
     }, tempo );
@@ -19,11 +27,26 @@ function esperaAi (msg, tempo ) {
 
 }
 
-esperaAi ('Frase 1', rand(1, 3))
-.the(resposta => {
+//then() > resolve ->  A função resolve é chamada quando a operação é concluída com sucesso.
+//catch() > reject ->  e a função reject é chamada quando a operação falha. 
+//Uma promise é um objeto em JavaScript que representa a conclusão ou falha de uma operação assíncrona. 
+//Para criar uma promise, é possível usar a palavra-chave new e passar uma função que recebe dois argumentos: resolve e reject.
+
+esperaAi ('Conexão com o BD', rand(1, 3))
+.then(resposta => {
     console.log( resposta);
-    return esperaAi ('Frase 2', rand(1, 3));
-})
+    return esperaAi ('Buscando dados da BASE', rand(1, 3));
+}).then(resposta => {
+    console.log(resposta ); 
+    return esperaAi (222222, rand(1, 3));
+}).then(resposta => {
+    console.log(resposta);
+    return esperaAi('Exibi dados na tela'); 
+}).then(resposta => {
+    console.log(resposta);
+}).catch( e => {
+    console.log('ERRO:', e);
+});
 
 
 
