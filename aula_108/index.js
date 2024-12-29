fbbf96b2c5e16a10442ec1ed17ebeb23ea3a2adc
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2024-12-29 18:53:34 -0300

    🚧
    index.js

---
+++ aula_108/index.js
@@ -0,0 +1,29 @@
+//Promises > promessas foi criada para facilitar as callback 
+
+function rand(min , max){
+
+    min *= 1000; 
+    max *= 1000;
+
+    return Math.floor(Math.random() * (max - min) + min );
+}
+
+
+function esperaAi (msg, tempo ) {
+    return new Promise(( resolve, reject) => {
+        setTimeout(( ) => {
+            resolve(msg)
+    }, tempo );
+
+    });
+
+}
+
+esperaAi ('Frase 1', rand(1, 3))
+.the(resposta => {
+    console.log( resposta);
+    return esperaAi ('Frase 2', rand(1, 3));
+})
+
+
+
