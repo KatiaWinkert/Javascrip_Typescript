c6ecf5fb7f58a56436cdf68f65e2f4011ed5ad37
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2024-12-31 00:43:10 -0300

    🚧 Class 110 Async  and await
    aula onde explica a funcionalidade de async e await em promises , suas vantagens e como deixa o codigo mais limpo. Últimos commits do ano de 2024 :)

--- aula_110/async.js
+++ aula_110/async.js
@@ -2,7 +2,7 @@
 
 
 //função rand executa o tempo 
-function rand(min , max){
+function rand(min=0 , max=3){
 
     min *= 1000; 
     max *= 1000;
@@ -20,17 +20,48 @@ function rand(min , max){
 function esperaAi (msg, tempo ) {
     return new Promise(( resolve, reject) => {
 
-    setTimeout(( ) => {
         if(typeof msg !== 'string') {
              reject ('cai no error');
              return;
             
         }
-        
+        setTimeout(( ) => {
             resolve(msg.toUpperCase() + ' - Recebi a promise');
             return;
 
     }, tempo );
 
     });
+
 }
+
+// esperaAi("Fase 1" , rand())
+// .then(valor => { console.log(valor)
+//     return esperaAi("fase 2", rand());
+// }).then(fase => {
+//     console.log(fase);
+//     return esperaAi("Fase 3", rand());
+// }).then(fase => {
+//     console.log('fase');
+// }).catch(e => console.log(e))
+
+async function executa() {
+
+    try{
+    const fase1 = await esperaAi('fase 1', rand());
+    console.log(fase1);
+
+    const fase2 = await esperaAi('fase 2', rand() );
+    console.log(fase2);
+
+    const fase3 = await esperaAi('fase 3', rand());
+    console.log(fase3);
+
+    console.log('Terminamos a fase:', fase3);
+    
+} catch(e){
+    console.log(e)
+}
+
+}
+executa()
