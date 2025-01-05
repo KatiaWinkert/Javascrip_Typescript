fb9429485ce190a3a1b2351a8da9862ce32e40bb
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2025-01-05 16:34:58 -0300

    💻 Compilador tradudor bundle
    Compilador tradutor de cod bundle

---
+++ aula_115_WebPack/public/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//1
+//2
+//3
+console.log('OIIIIIIIIIIIIIIIIIIIIIIIII');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
