3fca8e71ee681e402048eaff598a849dd2e3360e
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2025-01-11 00:20:59 -0300

    🚧 FormGerasenha
    variaveis  e processos de implementação com o html. Ação Click, função que gera senha e retorna o valor para o html

--- aula118_webpack/src/modules/formGerasenha.js
+++ aula118_webpack/src/modules/formGerasenha.js
@@ -1,5 +1,34 @@
-import geraSenha from "./geradores.js";
+import geraSenha from './geradores'; 
 
-export default () => {
-    console.log('Tudo bem?')
+
+const senhaGerada = document.querySelector('.senha-gerada');
+const qtdCaracteres = document.querySelector('.qtd-caracteres');
+const chkMaiusculas = document.querySelector('.chk-maiusculas');
+const chkMinusculas = document.querySelector('.chk-minusculas');
+const chknumeros = document.querySelector('.chk-numeros');
+const chksimbolos = document.querySelector('.chk-simbolos');
+
+const gerarSenha = document.querySelector('.gerar-senha');
+
+
+//click ação do botão gerar senha
+export default ( ) => {
+    gerarSenha.addEventListener('click', () => {
+        senhaGerada.innerHTML = gera()
+
+
+    });
+};
+
+//função gerando senha 
+function gera(){
+    const senha = geraSenha(
+        qtdCaracteres.value, // aqui peca a variavel e o valor que estara no input de cada um 
+        chkMaiusculas.checked,
+        chkMinusculas.checked,
+        chknumeros.checked,
+        chksimbolos.checked
+    );
+    return senha; //retorna o valor de cada input a cima. 
 }
+
