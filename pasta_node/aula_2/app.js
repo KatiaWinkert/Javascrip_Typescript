af62318b51c8eb5e44767e8655fcd7537949584e
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2025-01-12 22:12:01 -0300

    🚧 Atividade aula 2
    Treinando exportar e importar e a usar caminhos no node> comentários no arquivo.

---
+++ pasta_node/aula_2/app.js
@@ -0,0 +1,7 @@
+const Cachorro = require('./Z/mod2')
+
+const c1 = new Cachorro ('Jheyni')
+c1.latir()
+
+// ./ > é a pasta atual pra frente 
+// ../ > é a pasta voltando para sair e entrar em alguma pasta. 
