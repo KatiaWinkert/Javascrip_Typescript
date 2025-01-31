aa9a83990b5ba94d64f619226828a3dd8e58b1d6
Katia_Winkert <kwinkert.engthesoftware@gmail.com>
2025-01-30 23:21:21 -0300

    Update
    Update

--- pasta_node/aula4/index.js
+++ pasta_node/aula4/index.js
@@ -1,29 +1,31 @@
 //file System - Fs e Recursão mútua: 
 
-const fs = require('fs').promises; //fs não precisa fazer caminho pq ele ja tem
-const path = require ('path'); // path tambem não precisa de caminhos e ele será usado para tratar dos caminhos.
+const fs = require('fs').promises;
+const path = require('path');
 
-  async  function readdir(rootDir){
-        rootDir = rootDir || path.resolve(__dirname) ;
-        const files = await fs.readdir(rootDir);
-        walk(files, rootDir);
+async function readdir(rootDir) {
+  rootDir = rootDir || path.resolve(__dirname);
+  const files = await fs.readdir(rootDir);
+  walk(files, rootDir);
+}
 
+async function walk(files, rootDir) {
+  for(let file of files) {
+    const fileFullPath = path.resolve(rootDir, file);
+    const stats = await fs.stat(fileFullPath);
+
+    if (/\.git/g.test(fileFullPath)) continue;
+    if (/node_modules/g.test(fileFullPath)) continue;
+
+    if(stats.isDirectory()) {
+      readdir(fileFullPath);
+      continue;
     }
 
-    async function walk(files){
-        for(let file of files){
-            const fileFullPath = path.resolve(rootDir,file);
-            const stas = await fs.stat(fileFullPath);
-        
-            if(stas.isDirectory()){
-                readdir(fileFullPath);
-                continue;
-            }
-
-            console.log (file, stas.isDirectory )
-        }
-
-        
-    }
-
-    readdir('')
+    if (
+      !/\.html$/g.test(fileFullPath)
+    ) continue;
+    console.log(fileFullPath);
+  }
+}
+readdir('/media/luizotavio/Externo/JS/');
